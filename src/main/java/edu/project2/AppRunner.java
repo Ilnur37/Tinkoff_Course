cbde861aeb322e7f6924988a51efc88ae6a72454
Ilnur37 <dinmukhameto@mail.ru>
2023-11-13 23:18:02 +0300

    Project 2 | AppRunner refactor

--- src/main/java/edu/project2/AppRunner.java
+++ src/main/java/edu/project2/AppRunner.java
@@ -13,72 +13,100 @@ import lombok.NoArgsConstructor;
 @SuppressWarnings("RegexpSinglelineJava")
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AppRunner {
+    private static final int ALL_SOLUTIONS_METHODS = 3;
     private static final int MAX_SIZE = 30;
+    private static final String SIZE_MESSAGE = "Число должно быть больше 0 и меньше ";
     private static final String AXIS_X = "По оси X: ";
     private static final String AXIS_Y = "По оси Y: ";
-    private static final int ALL_SOLUTIONS_METHODS = 3;
+    private static final Scanner IN = new Scanner(System.in);
+    private static int row = 0;
+    private static int col = 0;
+    private static int startX = 0;
+    private static int startY = 0;
+    private static int endX = 0;
+    private static int endY = 0;
+    private static int solverType = 0;
 
     @SuppressWarnings("UncommentedMain")
     public static void main(String[] args) {
-        int row = 0;
-        int col = 0;
-        int startX = 0;
-        int startY = 0;
-        int endX = 0;
-        int endY = 0;
-        int solverType = 0;
+        Maze maze = createMaze();
+        Printer.printMaze(maze);
+        createStartAndEndPoint();
+        enterSolverType();
+        runSolver(maze);
+    }
 
-        Scanner in = new Scanner(System.in);
-        while (isNegative(row) || isInvalidSize(row)) {
-            System.out.println("Введите высоту лабиринта:");
-            row = in.nextInt();
-        }
-        while (isNegative(col) || isInvalidSize(col)) {
-            System.out.println("Введите ширину лабиринта:");
-            col = in.nextInt();
-        }
+    private static Maze createMaze() {
+        System.out.println("Введите высоту лабиринта:");
+        row = enterSize(row);
+        System.out.println("Введите ширину лабиринта:");
+        col = enterSize(col);
 
         System.out.println("Генерация лабиринта " + row + "X" + col + " с помощью алгоритма Эллера");
         GeneratorByEllerAlgorithm generator = new GeneratorByEllerAlgorithm();
-        Maze maze = generator.generate(row, col);
-        Printer.printMaze(maze);
+        return generator.generate(row, col);
+    }
 
+    private static int enterSize(int num) {
+        int tempNum = num;
+        while (isInvalidSize(tempNum)) {
+            System.out.println(SIZE_MESSAGE + MAX_SIZE);
+            tempNum = IN.nextInt();
+        }
+        return tempNum;
+    }
+
+    public static boolean isInvalidSize(int num) {
+        return num > MAX_SIZE || num <= 0;
+    }
+
+    private static void createStartAndEndPoint() {
         System.out.println("\nВыберите точку старта");
-        while (isInvalidPoint(startX, row)) {
-            System.out.println(AXIS_X);
-            startX = in.nextInt();
-        }
-        while (isInvalidPoint(startY, row)) {
-            System.out.println(AXIS_Y);
-            startY = in.nextInt();
-        }
-        System.out.println("\nВыберите точку финиша");
-        while (isInvalidPoint(endX, col)) {
-            System.out.println(AXIS_X);
-            endX = in.nextInt();
-        }
-        while (isInvalidPoint(endY, col)) {
-            System.out.println(AXIS_Y);
-            endY = in.nextInt();
-        }
+        System.out.println(AXIS_X);
+        startX = enterPoint(startX, col);
 
+        System.out.println(AXIS_Y);
+        startY = enterPoint(startY, row);
+
+        System.out.println("\nВыберите точку финиша");
+        System.out.println(AXIS_X);
+        endX = enterPoint(endX, col);
+
+        System.out.println(AXIS_Y);
+        endY = enterPoint(endY, row);
+    }
+
+    private static int enterPoint(int point, int border) {
+        int tempPoint = point;
+        while (isInvalidPoint(tempPoint, border)) {
+            System.out.println(SIZE_MESSAGE + border);
+            tempPoint = IN.nextInt();
+        }
+        return tempPoint;
+    }
+
+    public static boolean isInvalidPoint(int point, int border) {
+        return point > border || point <= 0;
+    }
+
+    private static void enterSolverType() {
         while (isInvalidTypeSolver(solverType)) {
             System.out.println("Выберите метод решения лабиринта:\t1 - DFS;\t2 - BFS;\t3-Оба");
-            solverType = in.nextInt();
+            solverType = IN.nextInt();
         }
+    }
+
+    public static boolean isInvalidTypeSolver(int num) {
+        return num != 1 && num != 2 && num != ALL_SOLUTIONS_METHODS;
+    }
+
+    private static void runSolver(Maze maze) {
         switch (solverType) {
-            case 1 -> {
-                solveDFS(maze, startX, startY, endX, endY);
-                break;
-            }
-            case 2 -> {
-                solveBFS(maze, startX, startY, endX, endY);
-                break;
-            }
+            case 1 -> solveDFS(maze, startX, startY, endX, endY);
+            case 2 -> solveBFS(maze, startX, startY, endX, endY);
             default -> {
                 solveDFS(maze, startX, startY, endX, endY);
                 solveBFS(maze, startX, startY, endX, endY);
-                break;
             }
         }
     }
@@ -93,21 +121,5 @@ public class AppRunner {
         Solver solverBFS = new SolverBFS(new Coordinate(startX, startY), new Coordinate(endX, endY));
         List<Coordinate> path = solverBFS.solve(maze);
         Printer.printMazeWithPath(maze, path);
-    }
-
-    private static boolean isInvalidSize(int num) {
-        return num > MAX_SIZE;
-    }
-
-    private static boolean isNegative(int num) {
-        return num <= 0;
-    }
-
-    private static boolean isInvalidPoint(int point, int border) {
-        return point > border || isNegative(point);
-    }
-
-    private static boolean isInvalidTypeSolver(int num) {
-        return num != 1 && num != 2 && num != ALL_SOLUTIONS_METHODS;
     }
 }
